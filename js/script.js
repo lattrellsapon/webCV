86c3a76255a211b774264cf1310bea453830cbc2
lattrellsapon <lattrellsapon@gmail.com>
2019-01-02 20:45:29 +1300

    Adding all of the files that I have been working on

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// $(document).ready(function()
+// {
+//     $("#webDevSkills").click(function()
+//     {
+//         $("#hide").fadeOut();
+//     });
+// });
+
+// $(document).ready(function()
+// {
+//     $("#webDevSkills").click(function()
+//     {
+//         $("#hide").fadeIn();
+//     });
+// });
+
+
