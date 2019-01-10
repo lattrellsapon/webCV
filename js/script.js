824511e75a0f6b5e397d37316fc71b5614b30569
lattrellsapon <lattrellsapon@gmail.com>
2019-01-10 21:59:16 +1300

    Added show and hide buttons for the certificate

--- js/script.js
+++ js/script.js
@@ -1,38 +1,17 @@
-// $(document).ready(function()
-// {
-//     $("#webDevSkills").click(function()
-//     {
-//         $("#hide").fadeOut();
-//     });
-// });
-
-// $(document).ready(function()
-// {
-//     $("#webDevSkills").click(function()
-//     {
-//         $("#hide").fadeIn();
-//     });
-// });
-
-
 $("#showCertificateButton").click( ()=>
 {   
     $(".certificates-wrapper").fadeIn(1000);
     $(".certificates-wrapper").css("display", "grid");
     
     $("#showCertificateButton").hide();
-    $("#hideCertificateButton").css("display", "inline");
+    $("#hideCertificateButton").show();
+
 });
 
 $("#hideCertificateButton").click( ()=>
 {   
-    // $("#udemyCertificates").fadeOut();
     $(".certificates-wrapper").fadeOut(1000);
-    $("#hideCertificateButton").html("Show Certificates");
-    $("#hideCertificateButton").attr("id", "showCertificateButton");
+    $("#hideCertificateButton").hide();
+    $("#showCertificateButton").show();
 
-
-    
-    // $("#showCertificateButton").html("Hide Certificates");
-    // $("#showCertificateButton").attr("id", "hideCertificateButton");
 });
